79805453f8d17eb2c52a1a5ad8ab592b8bd57dd3
bakhtiyartayirov <bakhtiyartayirov@gmail.com>
2022-02-17 12:38:09 +0500

    created post type

--- themes/demografik/inc/post-types.php
+++ themes/demografik/inc/post-types.php
@@ -4,15 +4,15 @@ function wporg_register_taxonomy_course() {
      $labels = array(
          'name'              => _x( 'Infographic-cats', 'taxonomy general name' ),
          'singular_name'     => _x( 'Infographic-cat', 'taxonomy singular name' ),
-         'search_items'      => __( 'Search Infographic-cat' ),
-         'all_items'         => __( 'All Infographic-cat' ),
-         'parent_item'       => __( 'Parent Infographic-cat' ),
-         'parent_item_colon' => __( 'Parent Infographic-cat:' ),
-         'edit_item'         => __( 'Edit Infographic-cat' ),
-         'update_item'       => __( 'Update Infographic-cat' ),
-         'add_new_item'      => __( 'Add New Infographic-cat' ),
-         'new_item_name'     => __( 'New Infographic-cat Name' ),
-         'menu_name'         => __( 'Infographic category' ),
+         'search_items'      => __( 'Search Infographic-cat', 'demografik' ),
+         'all_items'         => __( 'All Infographic-cat', 'demografik' ),
+         'parent_item'       => __( 'Parent Infographic-cat', 'demografik' ),
+         'parent_item_colon' => __( 'Parent Infographic-cat', 'demografik' ),
+         'edit_item'         => __( 'Edit Infographic-cat', 'demografik' ),
+         'update_item'       => __( 'Update Infographic-cat', 'demografik' ),
+         'add_new_item'      => __( 'Add New Infographic-cat', 'demografik' ),
+         'new_item_name'     => __( 'New Infographic-cat Name', 'demografik' ),
+         'menu_name'         => __( 'Infographic category', 'demografik' ),
      );
      $args   = array(
          'hierarchical'      => true, // make it hierarchical (like categories)
@@ -37,11 +37,148 @@ function wporg_custom_post_type() {
             'menu_icon'   => 'dashicons-format-gallery',
             'has_archive' => true,
             'taxonomies' => ['infographic-cat'],
-            'supports'   => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'meta_box' ),
+            'supports'   => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
         )
     );
 }
 add_action('init', 'wporg_custom_post_type');
+
+// Demografik pasport post type 
+
+function register_taxonomy_demografik() {
+     $labels = array(
+         'name'              => _x( 'categories', 'taxonomy general name' ),
+         'singular_name'     => _x( 'category', 'taxonomy singular name' ),
+         'search_items'      => __( 'Search categories', 'demografik' ),
+         'all_items'         => __( 'All category', 'demografik' ),
+         'parent_item'       => __( 'Parent category', 'demografik' ),
+         'parent_item_colon' => __( 'Parent category', 'demografik' ),
+         'edit_item'         => __( 'Edit category', 'demografik' ),
+         'update_item'       => __( 'Update category', 'demografik' ),
+         'add_new_item'      => __( 'Add new category', 'demografik' ),
+         'new_item_name'     => __( 'New category name', 'demografik' ),
+         'menu_name'         => __( 'Category', 'demografik' ),
+     );
+     $args   = array(
+         'hierarchical'      => true, // make it hierarchical (like categories)
+         'labels'            => $labels,
+         'show_ui'           => true,
+         'show_admin_column' => true,
+         'query_var'         => true,
+         'rewrite'           => [ 'slug' => 'demografik-category' ],
+     );
+     register_taxonomy( 'demografik-category', [ 'demografik-pasport' ], $args );
+}
+add_action( 'init', 'register_taxonomy_demografik' );
+
+function demogrf_custom_post_type() {
+    register_post_type('demografik-pasport',
+        array(
+            'labels'      => array(
+                'name'          => __('Demografik pasport', 'demografik'),
+                'singular_name' => __('Demografik pasport', 'demografik'),
+            ),
+            'public'      => true,
+            'menu_icon'   => 'dashicons-admin-site-alt2',
+            'has_archive' => true,
+            'taxonomies' => ['demografik-category'],
+            'supports'   => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
+        )
+    );
+}
+add_action('init', 'demogrf_custom_post_type');
+
+
+// Ragamli kutubxona post type 
+
+function register_taxonomy_library() {
+     $labels = array(
+         'name'              => _x( 'categories', 'taxonomy general name' ),
+         'singular_name'     => _x( 'category', 'taxonomy singular name' ),
+         'search_items'      => __( 'Search categories', 'demografik' ),
+         'all_items'         => __( 'All category', 'demografik' ),
+         'parent_item'       => __( 'Parent category', 'demografik' ),
+         'parent_item_colon' => __( 'Parent category', 'demografik' ),
+         'edit_item'         => __( 'Edit category', 'demografik' ),
+         'update_item'       => __( 'Update category', 'demografik' ),
+         'add_new_item'      => __( 'Add new category', 'demografik' ),
+         'new_item_name'     => __( 'New category name', 'demografik' ),
+         'menu_name'         => __( 'Category', 'demografik' ),
+     );
+     $args   = array(
+         'hierarchical'      => true, // make it hierarchical (like categories)
+         'labels'            => $labels,
+         'show_ui'           => true,
+         'show_admin_column' => true,
+         'query_var'         => true,
+         'rewrite'           => [ 'slug' => 'library-category' ],
+     );
+     register_taxonomy( 'library-category', [ 'library' ], $args );
+}
+add_action( 'init', 'register_taxonomy_demografik' );
+
+function library_custom_post_type() {
+    register_post_type('library',
+        array(
+            'labels'      => array(
+                'name'          => __('Library', 'demografik'),
+                'singular_name' => __('Library', 'demografik'),
+            ),
+            'public'      => true,
+            'menu_icon'   => 'dashicons-book-alt',
+            'has_archive' => true,
+            'taxonomies' => ['library-category'],
+            'supports'   => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
+        )
+    );
+}
+add_action('init', 'library_custom_post_type');
+
+
+// Xalqaro demografiya jurnali post type 
+
+function register_taxonomy_international_mag() {
+     $labels = array(
+         'name'              => _x( 'categories', 'taxonomy general name' ),
+         'singular_name'     => _x( 'category', 'taxonomy singular name' ),
+         'search_items'      => __( 'Search categories', 'demografik' ),
+         'all_items'         => __( 'All category', 'demografik' ),
+         'parent_item'       => __( 'Parent category', 'demografik' ),
+         'parent_item_colon' => __( 'Parent category', 'demografik' ),
+         'edit_item'         => __( 'Edit category', 'demografik' ),
+         'update_item'       => __( 'Update category', 'demografik' ),
+         'add_new_item'      => __( 'Add new category', 'demografik' ),
+         'new_item_name'     => __( 'New category name', 'demografik' ),
+         'menu_name'         => __( 'Category', 'demografik' ),
+     );
+     $args   = array(
+         'hierarchical'      => true, // make it hierarchical (like categories)
+         'labels'            => $labels,
+         'show_ui'           => true,
+         'show_admin_column' => true,
+         'query_var'         => true,
+         'rewrite'           => [ 'slug' => 'international-category' ],
+     );
+     register_taxonomy( 'inter-mag-category', [ 'inter_magazine' ], $args );
+}
+add_action( 'init', 'register_taxonomy_international_mag' );
+
+function inter_custom_post_type() {
+    register_post_type('inter_magazine',
+        array(
+            'labels'      => array(
+                'name'          => __('International journal', 'demografik'),
+                'singular_name' => __('International journal', 'demografik'),
+            ),
+            'public'      => true,
+            'menu_icon'   => 'dashicons-welcome-write-blog',
+            'has_archive' => true,
+            'taxonomies' => ['inter-mag-category'],
+            'supports'   => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
+        )
+    );
+}
+add_action('init', 'inter_custom_post_type');
 
 
 /**
